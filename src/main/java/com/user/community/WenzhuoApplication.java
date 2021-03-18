e110190eb189c0d7815c3349b759aaf3bd569e1b
zwz-kilig <zwz_305@163.com>
2021-03-18 21:07:08 +0800

    init repo

---
+++ src/main/java/com/user/community/WenzhuoApplication.java
@@ -0,0 +1,13 @@
+package com.user.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WenzhuoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WenzhuoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/user/community/WenzhuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WenzhuoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
