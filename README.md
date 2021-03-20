acff68b7fd1fe16e2b1c715617f4949c363e00d3
zwz-kilig <zwz_305@163.com>
2021-03-20 13:08:27 +0800

    msg

---
+++ README.md
@@ -0,0 +1,11 @@
+##  Spring Boot
+我的一个项目
+
+## 资料
+https://git-scm.com/download/win
+
+http://localhost:8080
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+## 资料
+
+## 工具
---
+++ src/main/java/com/community/user/WenzhuoApplication.java
@@ -0,0 +1,13 @@
+package com.community.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WenzhuoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WenzhuoApplication.class, args);
+    }
+
+}
