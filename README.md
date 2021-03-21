00dba55c9f986054ab31f8005f16357d68e97199
zwz-kilig <zwz_305@163.com>
2021-03-21 10:50:02 +0800

    rediect

--- README.md
+++ README.md
@@ -2,11 +2,10 @@
 我的一个项目
 
 ## 资料
-https://git-scm.com/download/win
+[git 文档](https://git-scm.com/download/win/)
 
 http://localhost:8080
 
-http://localhost:8080
 
 ## 资料
 
---
+++ src/main/java/com/community/user/controller/AuthorizeController.java
@@ -0,0 +1,40 @@
+package com.community.user.controller;
+
+import com.community.user.dto.AccessTokenDTO;
+import com.community.user.dto.GithubUser;
+import com.community.user.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String assessToken = githubProvider.getAssessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(assessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
