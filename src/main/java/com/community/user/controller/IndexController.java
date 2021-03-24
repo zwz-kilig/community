276d7c5623afde667c06ba32b7eabb54a381beed
zwz-kilig <zwz_305@163.com>
2021-03-24 11:18:03 +0800

    add flyway

--- src/main/java/com/community/user/controller/IndexController.java
+++ src/main/java/com/community/user/controller/IndexController.java
@@ -24,7 +24,6 @@ public class IndexController {
         for (Cookie cookie : cookies) {
             if(cookie.getName().equals("token")){
                 String token = cookie.getValue();
-
                 User user = userMapper.findByToken(token);
                 if (user != null){
                     request.getSession().setAttribute("user", user);
